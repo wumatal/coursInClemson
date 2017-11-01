2f562682634c573a2a1f8a7ad9a1c861a3ee604e
Yupeng Wu <yupengw@ada6.cs.clemson.edu>
2017-10-31 23:43:44 -0400

    add 405 assn2 & program practice problems

---
+++ prgmPracTm/1dRaindrop/sol-1/main.cpp
@@ -0,0 +1,60 @@
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <sstream>
+
+struct bar{
+  int height;
+  int pos;
+} b;
+
+int main() {
+  std::ifstream file("input.txt");
+  std::string str;
+  int n;
+  int drops;
+  int pos;
+  int height;
+  std::vector<bar> bars;
+
+  while (std::getline(file, str)) {
+    drops = 0;
+    pos = 0;
+    height = 0;
+    if ( !bars.empty() ) {
+      bars.clear();
+    }
+    std::stringstream stream(str);
+    while(stream >> n){
+      std::cout << "pos" << pos << " : "<< std::endl;
+      if ( bars.empty() || (bars.back().height > n)){
+        b.height = n;
+        b.pos = pos;
+        bars.push_back(b);
+      }
+      else {
+        while( bars.back().height <= n ){
+          drops += (bars.back().height - height) * (pos - bars.back().pos -1);
+          height = bars.back().height;
+          bars.pop_back();
+          if ( bars.empty()){
+            b.height = n;
+            b.pos = pos;
+            bars.push_back(b);
+            break;
+          }
+        }
+      }
+      pos ++ ;
+      std::cout << " the bars : ";
+      for(std::vector<bar>::const_iterator i = bars.begin(); i != bars.end(); i++){
+        std::cout << i->height << ' ';
+        //std::cout << "(n: " << n << ") ";
+
+      }
+      std::cout << std::endl;
+      std::cout << drops << std::endl;
+    }
+  }
+}
