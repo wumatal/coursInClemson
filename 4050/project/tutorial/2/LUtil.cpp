d28a2c1bdf6466d80895283b28381dacacb9f896
Yupeng Wu <yupengw@ada2.cs.clemson.edu>
2017-11-16 16:00:26 -0500

    new changes

--- 4050/project/tutorial/2/LUtil.cpp
+++ 4050/project/tutorial/2/LUtil.cpp
@@ -10,6 +10,9 @@ bool initGL() {
   // Initialize Projection Matrix
   glMatrixMode( GL_PROJECTION );
   glLoadIdentity();
+  // multiply current Matrix against an orthographic perspective matrix with
+  // the l, r, b, t, n and f in the args.
+  // by doing so, set (0,0) to top-left of the window
   glOrtho( 0.0, SCREEN_WIDTH, SCREEN_HEIGHT, 0.0, 1.0, -1.0);
 
   // Initialize Modelview Matrix
@@ -27,4 +30,19 @@ bool initGL() {
   }
 
   return true;
+}
+
+void update() {
+
+}
+void render() {
+  // Clear color buffer
+  glClear( GL_COLOR_BUFFER_BIT );
+
+  // Reset modelview matrix
+  glMatrixMode( GL_MODELVIEW );
+  glLoadIdentity();
+
+  // Move to center of the screen
+  glTranslatef( SCREEN_WIDTH / 2.f, SCREEN_HEIGHT / 2.f, 0.f);
 }
