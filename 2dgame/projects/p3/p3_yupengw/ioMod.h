00d2878f6254162b6cbbf168e60fec31fc855632
wumetal <wuyup@mail.ustc.edu.cn>
2018-03-03 05:11:51 -0500

    new changes

--- 2dgame/projects/p3/p3_yupengw/ioMod.h
+++ 2dgame/projects/p3/p3_yupengw/ioMod.h
@@ -2,6 +2,7 @@
 #include <string>
 #include <SDL.h>
 #include <SDL_ttf.h>
+#include "gamedata.h"
 
 class IoMod {
 public:
@@ -9,7 +10,11 @@ public:
   ~IoMod();
   SDL_Texture* readTexture(const std::string& filename);
   SDL_Surface* readSurface(const std::string& filename);
-  void writeText(const std::string&, int, int, SDL_Color = {176, 228, 197, 0}) const;
+  void writeText(const std::string&, int, int, 
+    SDL_Color = {(uint8_t)Gamedata::getInstance().getXmlInt("font/red"), 
+                 (uint8_t)Gamedata::getInstance().getXmlInt("font/green"), 
+                 (uint8_t)Gamedata::getInstance().getXmlInt("font/blue"), 
+                 (uint8_t)Gamedata::getInstance().getXmlInt("font/alpha")}) const;
   // void writeText(const std::string&, int, int) const;
   SDL_Renderer* getRenderer() const { return renderer; }
 private:
